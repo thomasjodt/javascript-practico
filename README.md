79514cf0a097fce59e0a5a746e9c7bab00d1f35b
thomasjodt <jodt5200@gmail.com>
2022-02-22 19:37:41 -0500

    Agregué valores fijos a las medidas de las figuras

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Repositorio del curso práctico de Javascript de Platzi
 - Segundo paso: Implementar las fórmulas en JS.
 - Tercer paso: Crear funciones.
 - Cuarto paso: Integrar Javascript con HTML.
+
+### Figuras Geométricas Parte 1
+He creado las operaciones para hallar los valores del perímetro y área de las distintas figuras geométricas con medidas fijas.
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,42 @@
-console.log("Hello World");
+// Código del cuadrado
+
+console.group("Cuadrado");
+
+    const sqrSide = 10;
+    console.log(`Los lados del cuadrado miden: ${sqrSide} cm`);
+
+    const sqrPerimeter = 4*sqrSide;
+    console.log(`El perímetro del cuadrado es: ${sqrPerimeter} cm`);
+
+    const sqrArea = Math.pow(sqrSide,2);
+    console.log(`El área del cuadrado es: ${sqrArea} cmˆ2`);
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulo");
+    const triangleSide1 = 10;
+    const triangleHeight = 8;
+    const triangleBase = 6;
+    console.log(`Los lados del triángulo miden: ${triangleSide1} cm, ${triangleHeight} cm y ${triangleBase} cm`);
+
+    const trianglePerimeter = triangleSide1 + triangleHeight + triangleBase;
+    console.log(`El perímetro del triángulo es: ${trianglePerimeter} cm`);
+
+    const triangleArea = (triangleBase*triangleHeight)/2;
+    console.log(`El área del triángulo es: ${triangleArea} cmˆ2`);
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculo");
+    const circleRadius = 4;
+    const circleDiameter = circleRadius*2;
+    const pi = Math.PI.toFixed(2);
+    console.log(`El radio de la circunferencia es: ${circleRadius} cm y su diámetro es: ${circleDiameter} cm`);
+
+    const circlePerimeter = circleDiameter*pi;
+    console.log(`El perímetro del círculo es: ${circlePerimeter}`);
+
+    const circleArea = pi*(Math.pow(circleRadius,2));
+    console.log(`El área del círculo es: ${circleArea} cmˆ2`);
+console.groupEnd();
