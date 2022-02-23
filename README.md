79c40f8507894c32ca2e967d1a2327ae6921083e
thomasjodt <jodt5200@gmail.com>
2022-02-22 20:13:43 -0500

    Cambié el código para que sea más dinámico

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Repositorio del curso práctico de Javascript de Platzi
 
 ### Figuras Geométricas Parte 1
 He creado las operaciones para hallar los valores del perímetro y área de las distintas figuras geométricas con medidas fijas.
-
+### Figuras Geométricas Parte 2
+En esta sección he Cambiado gran parte del código para reemplazar las variables por funciones y de esa manera hacer medidas que puedan cambiar y no estén fijas.
--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,24 @@
 // Código del cuadrado
+function sqrPerimeter(side){
+    return `${side*4} cm `;
+}
+function sqrArea(side){
+    return `${Math.pow(side,2)} cm^2`;
+}
 
-console.group("Cuadrado");
+// Código del Triángulo
+function trianglePerimeter(side, base, height){
+    return `El perímetro del triángulo es: ${side + base + height} cm`;
+}
+function triangleArea(side, base, height) { 
+    return `El área del triángulo es: ${(base*height)/2} cm`;
+}
 
-    const sqrSide = 10;
-    console.log(`Los lados del cuadrado miden: ${sqrSide} cm`);
-
-    const sqrPerimeter = 4*sqrSide;
-    console.log(`El perímetro del cuadrado es: ${sqrPerimeter} cm`);
-
-    const sqrArea = Math.pow(sqrSide,2);
-    console.log(`El área del cuadrado es: ${sqrArea} cmˆ2`);
-
-console.groupEnd();
-
-// Código del triángulo
-console.group("Triángulo");
-    const triangleSide1 = 10;
-    const triangleHeight = 8;
-    const triangleBase = 6;
-    console.log(`Los lados del triángulo miden: ${triangleSide1} cm, ${triangleHeight} cm y ${triangleBase} cm`);
-
-    const trianglePerimeter = triangleSide1 + triangleHeight + triangleBase;
-    console.log(`El perímetro del triángulo es: ${trianglePerimeter} cm`);
-
-    const triangleArea = (triangleBase*triangleHeight)/2;
-    console.log(`El área del triángulo es: ${triangleArea} cmˆ2`);
-console.groupEnd();
 
 // Código del círculo
-console.group("Círculo");
-    const circleRadius = 4;
-    const circleDiameter = circleRadius*2;
-    const pi = Math.PI.toFixed(2);
-    console.log(`El radio de la circunferencia es: ${circleRadius} cm y su diámetro es: ${circleDiameter} cm`);
-
-    const circlePerimeter = circleDiameter*pi;
-    console.log(`El perímetro del círculo es: ${circlePerimeter}`);
-
-    const circleArea = pi*(Math.pow(circleRadius,2));
-    console.log(`El área del círculo es: ${circleArea} cmˆ2`);
-console.groupEnd();
+function circlePerimeter (radius) {
+    return `El perímetro del círculo es: ${(Math.PI*radius*2).toFixed(2)} cm`;
+}
+function circleArea (radius) {
+    return `El área del círculo es: ${(Math.PI*(Math.pow(radius,2))).toFixed(2)} cm`
+}
